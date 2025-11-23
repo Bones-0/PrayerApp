efd933099934e84a54054fd8a0f8e348b78f0528
Bones <pentagonzero7@gmail.com>
2025-11-23 16:18:04 +0300

    Added input field

--- main.py
+++ main.py
@@ -4,9 +4,19 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols = 2
+        self.Label_input("name", "Name:", False)
+    def Label_input(self, variable_name, label_text, multipleline=False):
+        self.add_widget(Label(text=label_text))
+        self.variable_name = TextInput(multiline=multipleline)
+        self.add_widget(self.variable_name)
+
 class MyApp(App):
     def build(self):
-        return Label(text="Prayer App")
+        return MyGrid()
 
 
 if __name__ == "__main__":
